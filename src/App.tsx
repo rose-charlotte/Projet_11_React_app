cd51794291c7c4d8a4dd3b6409af62aebe12d1b3
momilette <charlotte@anros.fr>
2024-02-13 12:29:15 +0100

    mise en place du layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./Pages/Home/Home";
+import { Logement } from "./Pages/Logement/Logement";
+import { About } from "./Pages/About/About";
+import { Page404 } from "./Pages/Page404/Page404";
+import { Layout } from "./Pages/Layout/Layout";
 
-
-
-import {Routes, Route} from "react-router-dom"
-import { Home } from "./Pages/Home/Home"
-import { Logement } from "./Pages/Logement/Logement"
-import { About } from "./Pages/About/About"
-import { Page404 } from "./Pages/Page404/Page404"
-
-
-function App() {  
- return (
-  <Routes>
-    <Route path="/" element={< Home/>} />
-    <Route path="/logement" element={<Logement />} />
-    <Route path="/about" element={<About />}/>
-    <Route path="/404" element={<Page404 />}/>
-    <Route path="*" element={<Page404 />} />
-  </Routes>
- )
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="logement" element={<Logement />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Page404 />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
