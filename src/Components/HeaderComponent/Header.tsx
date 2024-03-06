1708d40d6dec3a438af80250f05ff26abe89280e
momilette <charlotte@anros.fr>
2024-03-06 16:55:45 +0100

    mise en place Header

--- src/Components/HeaderComponent/Header.tsx
+++ src/Components/HeaderComponent/Header.tsx
@@ -4,13 +4,21 @@ import { Link } from "react-router-dom";
 export function Header() {
   return (
     <div className={style.container}>
-      <img src="src/assets/D_LOGO.png" alt="Logo Kasa" />
+      <picture>
+        <source srcSet="src/assets/D_LOGO.png" media="(min-width: 481px)" />
+        <img src="src/assets/M_LOGO.png" alt="Logo Kasa" />
+      </picture>
+
       <ul className={style.listContainer}>
         <li className={style.listItem}>
-          <Link to="/">Accueil</Link>
+          <Link className={style.link} to="/">
+            Accueil
+          </Link>
         </li>
-        <li>
-          <Link to="/about">A Propos</Link>
+        <li className={style.listItem}>
+          <Link className={style.link} to="/about">
+            A Propos
+          </Link>
         </li>
       </ul>
     </div>
