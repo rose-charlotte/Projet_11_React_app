db41130e8c9d7c92734253191667f555a7db4d4f
momilette <charlotte@anros.fr>
2024-03-11 15:22:58 +0100

    mise en place de l'accordeon + animation

---
+++ src/Components/Accordeon/Accordeon.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import style from "./Accordeon.module.scss";
+
+export function Accordeon(props: DropDownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleDropdown() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className={style.container}>
+      <button onClick={toggleDropdown} className={style.arrow}>
+        <span className={style.title}>{props.title}</span>
+        {!isOpen && (
+          <img
+            className={style.img}
+            src="src/assets/images/arrowUp.png"
+            alt="arrow"
+          />
+        )}
+        {isOpen && (
+          <img
+            className={style.img}
+            src="src/assets/images/arrowDown.png"
+            alt="arrow"
+          />
+        )}
+      </button>
+
+      {isOpen && (
+        <div className={style.description}>
+          <div className={style.text}> {props.description}</div>
+        </div>
+      )}
+    </div>
+  );
+}
+export interface DropDownProps {
+  title: string;
+  description: string;
+}
