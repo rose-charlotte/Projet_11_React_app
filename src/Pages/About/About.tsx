47b67d26327a691e05bf22b22eec908f347768ed
momilette <charlotte@anros.fr>
2024-03-11 15:02:17 +0100

    mise en place about page

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,15 +1,42 @@
 import { Banner } from "../../Components/Banner/Banner";
-import { DropDown } from "../../Components/Dropdown/DropDown";
+import { Accordeon } from "../../Components/Accordeon/Accordeon";
 import style from "./About.module.scss";
 
 export function About() {
-  const titles = ["Fiabilité", "Respect", "Service", "Sécurité"];
+  const aboutElements = [
+    {
+      title: "Fiabilité",
+      description:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes",
+    },
+    {
+      title: "Respect",
+      description:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme ",
+    },
+    {
+      title: "Service",
+      description:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme ",
+    },
+
+    {
+      title: "Sécurité",
+      description:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
   return (
     <div className={style.container}>
       <Banner source="src/assets/images/bannerAbout.png" />
       <div className={style.dropDownContainer}>
-        {titles.map((title) => (
-          <DropDown title={title} key={title} />
+        {aboutElements.map((element) => (
+          <Accordeon
+            title={element.title}
+            description={element.description}
+            key={element.title}
+          />
         ))}
       </div>
     </div>
