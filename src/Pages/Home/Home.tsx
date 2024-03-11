1a2ee88d8f0f3bac30e9996832c76367201ba00f
momilette <charlotte@anros.fr>
2024-03-11 11:42:44 +0100

    modif page Home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -6,10 +6,12 @@ import data from "../../assets/Data/data.json";
 export function Home() {
   return (
     <div className={style.container}>
-      <Banner
-        source="src/assets/images/bannerHome.png"
-        text="Chez vous, partout et ailleurs"
-      />
+      <div className={style.bannerHome}>
+        <Banner
+          source="src/assets/images/bannerHome.png"
+          text="Chez vous, partout et ailleurs"
+        />
+      </div>
       <div className={style.cardsContainer}>
         {data.map((data) => (
           <Card title={data.title} cover={data.cover} key={data.id} />
