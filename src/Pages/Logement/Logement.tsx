d74e014ee4e4131f93a5cbd99b12fcdcbabe4945
momilette <charlotte@anros.fr>
2024-03-12 18:13:33 +0100

    mise en place logement page

--- src/Pages/Logement/Logement.tsx
+++ src/Pages/Logement/Logement.tsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router";
+import { Rating } from "../../Components/Rating/Rating.tsx";
+import { Accordeon } from "../../Components/Accordeon/Accordeon.tsx";
 import { Slideshow } from "../../Components/slideshow/Slideshow.tsx";
 import style from "./Logement.module.scss";
 
 import data from "../../assets/Data/data.json";
-import { Rating } from "../../Components/Rating/Rating.tsx";
-import { Accordeon } from "../../Components/Accordeon/Accordeon.tsx";
+import { Host } from "../../Components/Host/Host.tsx";
+import { Tag } from "../../Components/Tag/Tag.tsx";
+import { Infos } from "../../Components/Infos/Infos.tsx";
 
 export function Logement() {
   const { id } = useParams();
@@ -14,18 +17,14 @@ export function Logement() {
   return (
     <div className={style.container}>
       <Slideshow photos={dataElement!.pictures} />
-      <h1>{dataElement?.title}</h1>
-      <p>{dataElement?.location}</p>
-      {dataElement?.tags.map((tag) => (
-        <div className={style.tag} key={tag}>
-          {tag}
-        </div>
-      ))}
+      <Infos title={dataElement!.title} location={dataElement!.location} />
+      <Tag tags={dataElement!.tags} />
+
       <Rating />
-      <div className={style.accordeonCOntainer}>
-        <Accordeon title="Description" description={dataElement?.description} />
-        <Accordeon title="Equipement" list={dataElement?.equipments} />
-      </div>
+      <Host name={dataElement!.host.name} image={dataElement!.host.picture} />
+
+      <Accordeon title="Description" description={dataElement?.description} />
+      <Accordeon title="Equipement" list={dataElement?.equipments} />
     </div>
   );
 }
