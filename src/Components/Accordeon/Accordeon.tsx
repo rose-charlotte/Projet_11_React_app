cd333afc9eda18354d4f3e2188c142ceffc389ad
momilette <charlotte@anros.fr>
2024-03-12 18:40:33 +0100

    modif accordeon

--- src/Components/Accordeon/Accordeon.tsx
+++ src/Components/Accordeon/Accordeon.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import ArrowUp from "../../assets/images/ArrowUp.svg?react";
+import ArrowDown from "../../assets/images/ArrowDown.svg?react";
 import style from "./Accordeon.module.scss";
 
 export function Accordeon(props: DropDownProps) {
@@ -12,20 +14,8 @@ export function Accordeon(props: DropDownProps) {
     <div className={style.container}>
       <button onClick={toggleDropdown} className={style.arrow}>
         <span className={style.title}>{props.title}</span>
-        {!isOpen && (
-          <img
-            className={style.img}
-            src="src/assets/images/arrowUp.png"
-            alt="arrow"
-          />
-        )}
-        {isOpen && (
-          <img
-            className={style.img}
-            src="src/assets/images/arrowDown.png"
-            alt="arrow"
-          />
-        )}
+        {!isOpen && <ArrowUp />}
+        {isOpen && <ArrowDown />}
       </button>
 
       {isOpen && (
