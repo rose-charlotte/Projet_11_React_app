4b2b2bdf0acfc8d7a3c870a42b6505394efc31c9
momilette <charlotte@anros.fr>
2024-03-12 18:12:06 +0100

    composant Tag

---
+++ src/Components/Tag/Tag.tsx
@@ -0,0 +1,15 @@
+import style from "./Tag.module.scss";
+
+export function Tag(props: TagProps) {
+  return (
+    <div className={style.container}>
+      {props.tags.map((tag) => (
+        <div className={style.tag}>{tag}</div>
+      ))}
+    </div>
+  );
+}
+
+export interface TagProps {
+  tags: string[];
+}
