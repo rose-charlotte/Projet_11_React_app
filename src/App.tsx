5f5ed450848d2a67dedbacb4f0d7735a15e88b2c
momilette <charlotte@anros.fr>
2024-03-15 09:25:34 +0100

    mise en place de la page error

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Route,
+  Navigate,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import { Home } from "./Pages/Home/Home";
 import { Logement } from "./Pages/Logement/Logement";
 import { About } from "./Pages/About/About";
 import { Page404 } from "./Pages/Page404/Page404";
 import { Layout } from "./Pages/Layout/Layout";
+import { Error as ErrorPage } from "./Pages/Error/Error";
+import data from "./assets/Data/data.json";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={<Layout />}
+      errorElement={<Navigate to="/error" />}
+    >
+      <Route index element={<Home />} />
+      <Route
+        path="logement/:id"
+        element={<Logement />}
+        loader={({ params }) => {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const dataElement = data.find((data) => data.id === params.id);
+
+          if (!dataElement) {
+            throw new Error("Logement inconnu");
+          }
+
+          return dataElement;
+        }}
+      />
+      <Route path="about" element={<About />} />
+      <Route path="error" element={<ErrorPage />} />
+      <Route path="*" element={<Page404 />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="logement/:id" element={<Logement />} />
-        <Route path="about" element={<About />} />
-        <Route path="*" element={<Page404 />} />
-      </Route>
-    </Routes>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
