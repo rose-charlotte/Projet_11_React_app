3009b99ea6a782e784e0b23f6476557dfc66126a
momilette <charlotte@anros.fr>
2024-03-25 13:10:18 +0100

    Modifications CSS

--- src/Pages/Layout/Layout.tsx
+++ src/Pages/Layout/Layout.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../Components/Header/Header.tsx";
 import { Footer } from "../../Components/Footer/Footer.tsx";
+import style from "./Layout.module.scss";
 
 export function Layout() {
   return (
     <>
-      <Header />
-      <Outlet />
+      <div className={style.mainContentContainer}>
+        <Header />
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
